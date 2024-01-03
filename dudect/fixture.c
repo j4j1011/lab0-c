7afb2b70f12566bc34afa7c1b137c8f8b9c61e3b
Jerry <jerry2606142@gmail.com>
2024-01-03 10:19:14 +0000

    Fix doit() return bool

--- dudect/fixture.c
+++ dudect/fixture.c
@@ -157,11 +157,10 @@ static bool doit(int mode)
     prepare_inputs(input_data, classes);
     bool ret = measure(before_ticks, after_ticks, input_data, mode);
     differentiate(exec_times, before_ticks, after_ticks);
-    if (percentiles[N_MEASURES - 1])
+    if (percentiles[N_MEASURES - 1]) {
         prepare_percentiles(exec_times, percentiles);
-    ret = 0;
-    else
-    {
+        ret = 0;
+    } else {
         update_statistics(exec_times, classes, percentiles);
         ret &= report();
     }
